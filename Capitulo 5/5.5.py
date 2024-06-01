5c46048d3587036664d42b953c717941755ebdf3
Guilherme <guilhermestanger@gmail.com>
2024-06-01 14:51:29 -0300

    5.5

---
+++ Capitulo 5/5.5.py
@@ -0,0 +1,17 @@
+#Cores alienígenas #3
+
+
+#Definindo a cor do alienígena
+
+alien_colors = ['rosa'] # Altere para 'verde', 'amarelo' ou 'vermelho' para testar diferentes condições
+
+#Cadeia if-elif-else correspondente
+for alien_color in alien_colors:
+    if alien_color == 'verde':
+        print('Você acaba de ganhar 5 pontos por ser verde!')
+    elif alien_color == 'amarelo':
+         print('Você acaba de ganhar 10 pontos por ser amarelo!')
+    elif alien_color == 'vermelho':
+          print('Você acaba de ganhar 15 pontos por ser vermelho')
+    else: 
+        print('Cor errada!')
