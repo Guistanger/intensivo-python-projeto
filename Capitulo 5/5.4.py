3aadda06c9d04473d03414f1531916c238231542
Guilherme <guilhermestanger@gmail.com>
2024-06-01 14:34:33 -0300

    5.4

---
+++ Capitulo 5/5.4.py
@@ -0,0 +1,9 @@
+#Atribuindo valores à variaveis
+
+#Variavel if-else
+
+alien_color = ['verde']
+if 'verde' in alien_color:
+    print('Você acaba de ganhar 5 pontos por ser verde!')
+else: 
+    print('você acaba de ganhar 10 pontos por não ser verde')
