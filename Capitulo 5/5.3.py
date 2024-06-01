da977e24c560a24cdfebc6e816e1c84217af5ca1
Guilherme <guilhermestanger@gmail.com>
2024-06-01 13:57:24 -0300

    capitulo 5

---
+++ Capitulo 5/5.3.py
@@ -0,0 +1,17 @@
+#5.3 Cores de alienígenas #1
+
+#Atribuindo valores à variaveis
+
+alien_color = ['verde', 'amarelo', 'vermelho']
+if 'verde' in alien_color:
+    print('Você acaba de ganhar 5 pontos por ser verde!')
+
+#Versão que funcione e outra que falhe
+
+if 'vermelho' in alien_color:
+    print('Você escolheu a cor vermelha!')
+
+#Versão que falhe
+
+if 'rosa' in alien_color:
+    print('Não temos essa cor')
