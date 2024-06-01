2be9f6dbbc08837cbbc0d501ea02fb14fb0c07a7
Guilherme <guilhermestanger@gmail.com>
2024-06-01 15:07:16 -0300

    5.6

---
+++ Capitulo 5/5.6.py
@@ -0,0 +1,16 @@
+#Estágios da vida 5.6
+
+##cadeia if-elif-else que determine o estágio da vida de uma pessoa
+
+idade = 65  #local para definir idade
+
+if idade < 2:
+    print('Você é bebê!')
+elif 2 <= idade < 4:
+    print('Você é uma criança.')
+elif 4 <= idade < 13:
+    print('Você é jovem.')
+elif 20 <= idade < 65:
+    print('Você é um adulto.')
+elif 65 >= idade:
+    print('Você é um idoso')
